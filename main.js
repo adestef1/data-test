2a244bd911ff30be06f272bd5e59007af2736c53
Austin <austin_destefanis@brown.edu>
2022-03-07 17:43:27 -0500

    i hate it here

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-//  // set the dimensions and margins of the graph
-//  const margin = {top: 30, right: 30, bottom: 70, left: 60},
-//      width = 1000 - margin.left - margin.right,
-//      height = 400 - margin.top - margin.bottom;
+ // set the dimensions and margins of the graph
+ const margin = {top: 30, right: 30, bottom: 70, left: 60},
+     width = 1000 - margin.left - margin.right,
+     height = 400 - margin.top - margin.bottom;
  
-//  // append the svg object to the body of the page
-//  const svg = d3.select("#graph")
-//    .append("svg")
-//      .attr("width", width + margin.left + margin.right)
-//      .attr("height", height + margin.top + margin.bottom)
-//    .append("g")
-//      .attr("transform", `translate(${margin.left},${margin.top})`);
+ // append the svg object to the body of the page
+ const svg = d3.select("#graph")
+   .append("svg")
+     .attr("width", width + margin.left + margin.right)
+     .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+     .attr("transform", `translate(${margin.left},${margin.top})`);
  
-//  // Initialize the X axis
-//  const x = d3.scaleBand()
-//    .range([ 0, width ])
-//    .padding(0.2);
-//  const xAxis = svg.append("g")
-//    .attr("transform", `translate(0,${height})`)
+ // Initialize the X axis
+ const x = d3.scaleBand()
+   .range([ 0, width ])
+   .padding(0.2);
+ const xAxis = svg.append("g")
+   .attr("transform", `translate(0,${height})`)
  
-//  // Initialize the Y axis
-//  const y = d3.scaleLinear()
-//    .range([ height, 0]);
-//  const yAxis = svg.append("g")
-//    .attr("class", "myYaxis")
+ // Initialize the Y axis
+ const y = d3.scaleLinear()
+   .range([ height, 0]);
+ const yAxis = svg.append("g")
+   .attr("class", "myYaxis")
  
  
  // A function that create / update the plot for a given variable:
@@ -33,9 +33,6 @@
         let filteredData = data.filter(function(a) {
             return a[attr_col] == attr_val;
         });
-        // let questionData = filteredData.filter(function(a) {
-        //     return a[question];
-        // });
         console.log(filteredData)
         let hash = {};
         filteredData.forEach(function(a) {
@@ -46,31 +43,36 @@
                 hash[cleaned] = 1;
             }
         });
-        console.log(hash)
-        console.log("workpls")
+        let readable = []
+        for (const [key, value] of Object.entries(hash)) {
+            readable.append({'group': key, 'value': value});
+          }
+        console.log(readable)
+        console.log("HELLO")
 
+        //Get hash in readable format to than replace data with new thing, than should be easy hopefully?
  
-        // // Update the X axis
-        // x.domain(data.map(function(d) { return d.release_year;}))
-        // xAxis.call(d3.axisBottom(x))
-        
-        // // Update the Y axis
-        // y.domain([0, d3.max(data, d => d.value) ]);
-        // yAxis.transition().duration(1000).call(d3.axisLeft(y));
-        
-        // // Create the u variable
-        // var u = svg.selectAll("rect")
-        //     .data(data)
-        
-        // u
-        //     .join("rect") // Add a new rect for each new elements
-        //     .transition() 
-        //     .duration(1000)
-        //     .attr("x", d => x(d.group))
-        //     .attr("y", d => y(d.value))
-        //     .attr("width", x.bandwidth())
-        //     .attr("height", d => height - y(d.value))
-        //     .attr("fill", "#69b3a2")
+        // Update the X axis
+        x.domain(readable.map(d => d.group))
+        xAxis.call(d3.axisBottom(x))
+
+        // Update the Y axis
+        y.domain([0, d3.max(readable, d => d.value) ]);
+        yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+        // Create the u variable
+        var u = svg.selectAll("rect")
+            .data(readable)
+
+        u
+            .join("rect") // Add a new rect for each new elements
+            .transition() 
+            .duration(1000)
+            .attr("x", d => x(d.group))
+            .attr("y", d => y(d.value))
+            .attr("width", x.bandwidth())
+            .attr("height", d => height - y(d.value))
+            .attr("fill", "#69b3a2")
    })
  }
  
